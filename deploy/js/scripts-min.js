ec6041f29af52fb6391f5c7a1ebb7942873823fe
caseyreinknecht <81766422+caseyreinknecht@users.noreply.github.com>
2022-04-14 19:39:47 -0400

    parts 1&2

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,1 +1,19 @@
-!function(){"use strict";document.querySelector("#name").innerHTML="Your Name"}();
+import { gsap } from "gsap";
+
+document.querySelector("#Casey Reinknecht").innerHTML="Casey Reinknecht"; 
+
+
+
+
+const mainTL=gsap.timeline();
+mainTLPart1.from("box red", {duration:1, ease: "power1.out", x:0})
+.from("box blue", {duration:1, ease: "power1.out", x:350})
+.to("box red", {duration:2, ease: "power1.out", y:0})
+.to("box blue", {duration:2, ease: "power1.out", y:300})
+
+const mainTLPart2=gsap.timeline();
+gsap.to("graph", {duration: 2, Alpha:1})
+.from("box blue", {duration:1, ease: "power1.out", x:350})
+.to("box red", {duration:2, ease: "power1.out", y:0})
+.to("box blue", {duration:2, ease: "power1.out", y:300})
+.to("green", {duration:3, ease:"power1.out", x:400} )
