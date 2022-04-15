0697cb8690f85ead7566c50d96972eb2b3265955
caseyreinknecht <81766422+caseyreinknecht@users.noreply.github.com>
2022-04-14 20:06:01 -0400

    quiz fin

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,19 +1,7 @@
-import { gsap } from "gsap";
+//import { gsap } from "gsap";
 
-document.querySelector("#Casey Reinknecht").innerHTML="Casey Reinknecht"; 
+//const yourName = "Casey Reinknecht"
+//document.querySelector("Casey Reinknecht").innerHTML =yourName
+//const quiz =gsap.timeline()
 
 
-
-
-const mainTL=gsap.timeline();
-mainTLPart1.from("box red", {duration:1, ease: "power1.out", x:0})
-.from("box blue", {duration:1, ease: "power1.out", x:350})
-.to("box red", {duration:2, ease: "power1.out", y:0})
-.to("box blue", {duration:2, ease: "power1.out", y:300})
-
-const mainTLPart2=gsap.timeline();
-gsap.to("graph", {duration: 2, Alpha:1})
-.from("box blue", {duration:1, ease: "power1.out", x:350})
-.to("box red", {duration:2, ease: "power1.out", y:0})
-.to("box blue", {duration:2, ease: "power1.out", y:300})
-.to("green", {duration:3, ease:"power1.out", x:400} )
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,36 @@
 //import { gsap } from "gsap";
 
 //Replace this value with your name
-const yourName = "Your Name"
-document.querySelector("#name").innerHTML = yourName;
+const yourName = "Casey Reinknecht"
+document.querySelector("#Casey Reinknecht").innerHTML = yourName;
 
 //GreenSock Timeline
+
+const mainTL=gsap.timeline();
+mainTLPart1.from("box red", {duration:1, ease: "power1.out", x:0})
+.from("box blue", {duration:1, ease: "power1.out", x:350})
+.to("box red", {duration:2, ease: "power1.out", y:0})
+.to("box blue", {duration:2, ease: "power1.out", y:300})
+
+;
+
+const mainTLPart2=gsap.timeline.to("green", {duration:3, ease:"power1.out", x:400} )
+gsap.to("graph", {duration: 2, Alpha:0})
+.from("box red", {duration:1, ease: "power1.out", x:0})
+.from("box blue", {duration:1, ease: "power1.out", x:350})
+.to("box red", {duration:2, ease: "power1.out", y:0})
+.to("box blue", {duration:2, ease: "power1.out", y:300})
+.to("green", {duration:3, ease:"power1.out", x:400} )
+;
+
+const mainTLPart3=gsap.timeline.to("green", {duration:3, ease:"power1.out", x:400} )
+gsap.to("graph", {duration: 2, Alpha:0})
+.from("box red", {duration:1, ease: "power1.out", x:0})
+.from("box blue", {duration:1, ease: "power1.out", x:350})
+.to("graph", { duration:1, ease: "bounce.out",})
+.to("box red", {duration:2, ease: "power1.out", y:0})
+.to("box blue", {duration:2, ease: "power1.out", y:300})
+.to("box red", 2, {rotation:360, transformOrigin:"left top"})
+.to("box blue", 2, {rotation:360, transformOrigin:"left top"})
+
+;
